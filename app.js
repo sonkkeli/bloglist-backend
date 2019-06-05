9b37e32666211742f56cc3e329b35e34f6d2e3d9
sonkkeli <46299415+sonkkeli@users.noreply.github.com>
2019-06-05 15:53:56 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+
+const cors = require('cors')
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.get('/api', (req, res) => {
+    res.send('<p>continue to /api/blogs to see what you are looking for</p>')
+})
+
+app.use('/api/blogs', blogsRouter)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
